ab0470dc4764d11dcc0213f0b91c1f4d3ba269a0
FakerQu <qujiayu@outlook.com>
2024-08-01 20:43:01 +0800

    fix:ipv6地址获取不到最新问题

--- custom_components/alidns/__init__.py
+++ custom_components/alidns/__init__.py
@@ -1,8 +1,14 @@
 """Integrate with AliDNS."""
+from __future__ import annotations
 import asyncio
-from datetime import timedelta
+from ipaddress import IPv6Address
 import json
 import logging
+
+from collections.abc import Callable, Coroutine, Sequence
+from datetime import datetime, timedelta
+from typing import Any
+import ifaddr
 
 from aliyunsdkalidns.request.v20150109.AddDomainRecordRequest import (
     AddDomainRecordRequest,
@@ -21,6 +27,15 @@ import voluptuous as vol
 from homeassistant.const import CONF_DOMAIN, CONF_SCAN_INTERVAL
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components import network
+from homeassistant.core import (
+    CALLBACK_TYPE,
+    HassJob,
+    HomeAssistant,
+    callback,
+)
+from homeassistant.loader import bind_hass
+from homeassistant.util import dt as dt_util
+from homeassistant.helpers.event import async_call_later
 
 _LOGGER = logging.getLogger(__name__)
 TIMEOUT = 30  # seconds
@@ -31,6 +46,7 @@ DOMAIN = "alidns"
 CONF_ACCESS_ID = "access_id"
 CONF_ACCESS_KEY = "access_key"
 CONF_SUB_DOMAIN = "sub_domain"
+INTERVAL = timedelta(minutes=5)
 
 CONFIG_SCHEMA = vol.Schema(
     {
@@ -53,11 +69,10 @@ CONFIG_SCHEMA = vol.Schema(
 async def async_setup(hass, config):
     """Initialize the AliDNS component."""
     conf = config[DOMAIN]
-    access_id = conf.get(CONF_ACCESS_ID)
-    access_key = conf.get(CONF_ACCESS_KEY)
-    domain = conf.get(CONF_DOMAIN)
-    sub_domain = conf.get(CONF_SUB_DOMAIN)
-    update_interval = conf[CONF_SCAN_INTERVAL]
+    access_id = conf[CONF_ACCESS_ID]
+    access_key = conf[CONF_ACCESS_KEY]
+    domain = conf[CONF_DOMAIN]
+    sub_domain = conf[CONF_SUB_DOMAIN]
 
     loop = asyncio.get_running_loop()
 
@@ -72,27 +87,40 @@ async def async_setup(hass, config):
         """Update the AliDNS entry."""
         await _update_alidns(hass, acs_client, domain, sub_domain)
 
-    hass.helpers.event.async_track_time_interval(
-        update_domain_callback, update_interval
+    intervals = (
+        INTERVAL,
+        timedelta(minutes=1),
+        timedelta(minutes=5),
+        timedelta(minutes=15),
+        timedelta(minutes=30),
     )
+    async_track_time_interval_backoff(hass, update_domain_callback, intervals)
+
+    # hass.helpers.event.async_track_time_interval(
+    #     update_domain_callback, update_interval
+    # )
 
     return True
 
 
-async def _update_alidns(hass, acs_client, domain, sub_domain):
+async def _update_alidns(hass:HomeAssistant, acs_client, domain, sub_domain):
     """Update AliDNS."""
+    _LOGGER.warning(f"start update dns")
+    adapters = ifaddr.get_adapters()
+    for adapter in adapters:
+        for ip in adapter.ips:
+            _LOGGER.warning(f"ip from ifaddr {format(ip)}")
+
     try:
         with async_timeout.timeout(TIMEOUT):
-            adapters = await network.async_get_adapters(hass)
-            local_ip = ''
+            my_ip = ''
+            adapters = ifaddr.get_adapters()
             for adapter in adapters:
-                for ip_info in adapter["ipv6"]:
-                    if ip_info["address"].startswith('240e'):
-                        local_ip = ip_info["address"]
-                        _LOGGER.warning(f"ip address {local_ip}")
-            
-            # match = search(r"(([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4})|(([0-9a-fA-F]{1,4}:){6}:[0-9a-fA-F]{1,4})|(([0-9a-fA-F]{1,4}:){5}(:[0-9a-fA-F]{1,4}){1,2})|(([0-9a-fA-F]{1,4}:){4}(:[0-9a-fA-F]{1,4}){1,3})|(([0-9a-fA-F]{1,4}:){3}(:[0-9a-fA-F]{1,4}){1,4})|(([0-9a-fA-F]{1,4}:){2}(:[0-9a-fA-F]{1,4}){1,5})|([0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6}))|(:((:[0-9a-fA-F]{1,4}){1,7})))", body)
-            my_ip = local_ip
+                for ip in adapter.ips:
+                    if ip.is_IPv6:
+                        if ip.ip[0].startswith('240e'):
+                            my_ip = ip.ip[0]
+                            _LOGGER.warning(f"ip from ifaddr {format(ip)}")
             _LOGGER.warning(f"address {my_ip}")
 
             dns_record = None
@@ -149,3 +177,37 @@ async def _update_alidns(hass, acs_client, domain, sub_domain):
         _LOGGER.warning("Timeout to update alidns")
 
     return False
+
+@callback
+@bind_hass
+def async_track_time_interval_backoff(
+    hass: HomeAssistant,
+    action: Callable[[datetime], Coroutine[Any, Any, bool]],
+    intervals: Sequence[timedelta],
+) -> CALLBACK_TYPE:
+    """Add a listener that fires repetitively at every timedelta interval."""
+    remove: CALLBACK_TYPE | None = None
+    failed = 0
+
+    async def interval_listener(now: datetime) -> None:
+        """Handle elapsed intervals with backoff."""
+        nonlocal failed, remove
+        try:
+            failed += 1
+            if await action(now):
+                failed = 0
+        finally:
+            delay = intervals[failed] if failed < len(intervals) else intervals[-1]
+            remove = async_call_later(
+                hass, delay.total_seconds(), interval_listener_job
+            )
+
+    interval_listener_job = HassJob(interval_listener, cancel_on_shutdown=True)
+    hass.async_run_hass_job(interval_listener_job, dt_util.utcnow())
+
+    def remove_listener() -> None:
+        """Remove interval listener."""
+        if remove:
+            remove()
+
+    return remove_listener
